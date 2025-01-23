f4e247dc9ce170a235238260380de6f9996d5022
Sangjin Hwang <aosdb5640@naver.com>
2025-01-23 21:27:56 +0900

    add: button ui and audio

---
+++ src/AudioSource/index.js
@@ -0,0 +1,9 @@
+import book1 from "./book1.mp3";
+import book2 from "./book2.mp3";
+import book3 from "./book3.mp3";
+import book4 from "./book4.mp3";
+import book5 from "./book5.mp3";
+
+const AudioSource = [book1, book2, book3, book4, book5];
+
+export default AudioSource;
--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,21 +1,41 @@
 import { useRecoilState } from "recoil";
 import pageState from "../recoil/pageState";
 import bookSource from "../BookSource";
+import { useEffect, useRef, useState } from "react";
+import AudioSource from "../AudioSource";
 
 const UI = () => {
   const bookSrc = bookSource;
+  const audioSrc = AudioSource;
+  const audioRef = useRef();
   const [page, setPage] = useRecoilState(pageState);
+  const [audioIndex, setAudioIndex] = useState(-1);
+  const [audioState, setAudioState] = useState(true);
 
   const changePage = () => {
     const temp = Math.floor(Math.random() * bookSrc.length);
     setPage(temp);
+
+    const randomIndex = Math.floor(Math.random() * audioSrc.length);
+    setAudioIndex(randomIndex);
+    setAudioState((prev) => !prev);
   };
 
+  useEffect(() => {
+    if (audioIndex !== -1) {
+      audioRef.current.play();
+    }
+  }, [audioState]);
+
   return (
-    <div className="absolute flex bottom-56 w-full justify-center">
-      <button className="m-auto" onClick={changePage}>
-        Button
+    <div className="absolute flex bottom-52 w-full justify-center">
+      <button
+        className="m-auto font-Danjo text-gray-300 text-3xl"
+        onClick={changePage}
+      >
+        책님!
       </button>
+      <audio ref={audioRef} src={audioSrc[audioIndex]} />
     </div>
   );
 };
