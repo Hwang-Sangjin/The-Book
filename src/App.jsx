329b3e8cf1a43dea9012b2e371dd6e74cc562527
황상진 <aosdb5640@naver.com>
2025-01-26 23:50:52 +0900

    add: footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { RecoilRoot } from "recoil";
 import "./App.css";
 import Experience from "./components/Experience";
 import UI from "./components/UI";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <RecoilRoot>
       <Experience />
       <UI />
+      <Footer />
     </RecoilRoot>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return;
+  return (
+    <div className="absolute flex bottom-5 w-full justify-center">
+      <div className="m-3 ">Developer - Jin</div>
+      <div className="m-3">Original - Gamst</div>
+    </div>
+  );
 };
 
 export default Footer;
